5b561a462c589549a06feac1e5e2a087c89eded6
lmr87 <lmr87@DESKTOP-G9P7QE6>
2021-04-04 12:35:37 +0200

    Punto 10
    Creada la clase Consola, hago commit y push ahora porque necesito de el
    enumerado Opcion que es el siguiente punto de el ejercicio, volver
    despues a esta clase

---
+++ src/main/java/org/iesalandalus/programacion/biblioteca/mvc/vista/Consola.java
@@ -0,0 +1,57 @@
+package org.iesalandalus.programacion.biblioteca.mvc.vista;
+
+import java.time.LocalDate;
+
+import org.iesalandalus.programacion.biblioteca.mvc.modelo.dominio.Alumno;
+import org.iesalandalus.programacion.biblioteca.mvc.modelo.dominio.Libro;
+import org.iesalandalus.programacion.biblioteca.mvc.modelo.dominio.Prestamo;
+
+public class Consola {
+	/** Constructor por defecto */
+	private Consola() {
+		
+	}
+	
+	/** Método para mostrar menú */
+	public static void mostrarMenu() {
+		
+	}
+	
+	/** Método para mostrar cabecera */
+	public static void mostrarCabecera(String cabecera) {
+		
+	}
+	
+	/** Método para elegir una opción */
+	public static int elegirOpcion() {
+		
+	}
+	
+	public static Alumno leerAlumno() {
+		
+	}
+	
+	public static Alumno leerAlumnoFicticio() {
+		
+	}
+	
+	public static Libro leerLibro() {
+		
+	}
+	
+	public static Libro leerLibroFicticio() {
+		
+	}
+	
+	public static Prestamo leerPrestamo() {
+		
+	}
+	
+	public static Prestamo leerPrestamoFicticio() {
+		
+	}
+	
+	public static LocalDate leerFecha(String fecha) {
+		
+	}
+}
